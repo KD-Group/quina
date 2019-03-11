07a252fc2fecb22d16bbba7c62b67ae225526346
WingC <1018957763@qq.com>
2019-03-11 17:28:04 +0800

    remove test case for type check

--- tests/model/test_types.py
+++ tests/model/test_types.py
@@ -19,16 +19,10 @@ class MyTestCase(unittest.TestCase):
         mock_money = 3.26
         project.money.float.value = mock_money
         self.assertEqual(project.money.float.value, mock_money)
-        mock_wrong_type_money = '3.26'
-        with self.assertRaises(ValueError):
-            project.money.float.value = mock_wrong_type_money
 
         mock_ids = [1, 2, 1, 3, 8]
         project.ids.list.value = mock_ids
         self.assertEqual(project.ids.list.value, mock_ids)
-        mock_wrong_type_ids = (1, 2, 1, 3, 8)
-        with self.assertRaises(ValueError):
-            project.ids.list.value = mock_wrong_type_ids
 
         mock_books = [
             'Machine Learning',
@@ -36,9 +30,6 @@ class MyTestCase(unittest.TestCase):
         ]
         project.books.string_list.value = mock_books
         self.assertEqual(project.books.string_list.value, mock_books)
-        mock_wrong_type_books = ('Machine Learning', 'The Little Prince')
-        with self.assertRaises(ValueError):
-            project.books.string_list.value = mock_wrong_type_books
 
         mock_scores = {
             'math': 100,
@@ -47,9 +38,6 @@ class MyTestCase(unittest.TestCase):
         }
         project.scores.dict.value = mock_scores
         self.assertEqual(project.scores.dict.value, mock_scores)
-        mock_wrong_type_scores = [100, 98, 20]
-        with self.assertRaises(ValueError):
-            project.scores.dict.value = mock_wrong_type_scores
 
         self.assertEqual(project.value, {
             'money': mock_money,
@@ -62,9 +50,6 @@ class MyTestCase(unittest.TestCase):
         project.sell_time.time.update(t_format)
         mock_time = time.strftime(t_format)
         self.assertEqual(project.sell_time.time.value, mock_time)
-        mock_wrong_type_time = 1994.9
-        with self.assertRaises(ValueError):
-            project.sell_time.time.value = mock_wrong_type_time
 
 
 if __name__ == '__main__':
