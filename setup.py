78c43378a326eef419108e8f064487c06e8a3557
WingC <1018957763@qq.com>
2019-03-11 17:07:23 +0800

    update to v0.1.5

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='quina',
-    version='0.1.4',
+    version='0.1.5',
+raise ValueError(f'UnSupport Set Value Type {type(value)} to Type str')
     description='PySide2 MVVM Framework',
     url='https://github.com/KD-Group/quina',
     author='KD-Group',
