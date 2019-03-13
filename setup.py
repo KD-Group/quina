9fea61fcbf40c60b054be08b5463bf193a0f369d
WingC <1018957763@qq.com>
2019-03-13 17:18:02 +0800

    update to version 0.1.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='quina',
-    version='0.1.7',
+    version='0.1.8',
     description='PySide2 MVVM Framework',
     url='https://github.com/KD-Group/quina',
     author='KD-Group',
