2fdce62d3817eac2f3a7d291fb7b1c378c376d80
Liaolin <810608569@qq.com>
2019-06-18 13:06:21 +0800

    修复index=-1时索引越界

--- quina/widgets/widgets/table_widget.py
+++ quina/widgets/widgets/table_widget.py
@@ -45,7 +45,9 @@ class TableWidget(QTableWidget,
         self.string_list.check_change()
 
     def get_string_list_by_index(self, index: int):
-        if index < 0 or index >= self.row_count:
+        if index ==-1:
+            return None
+        elif index < 0 or index >= self.row_count:
             raise ValueError(f'Index Value {index} Out of Range')
         return [self.item(index, col).text() for col in range(self.column_count)]
 
