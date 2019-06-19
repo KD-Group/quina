05a15ad3f73ea66d2c78106223d11c33ea2a19bc
Liaolin <810608569@qq.com>
2019-06-19 16:58:55 +0800

    修复table_widget index=-1时索引越界

--- quina/widgets/widgets/table_widget.py
+++ quina/widgets/widgets/table_widget.py
@@ -45,10 +45,8 @@ class TableWidget(QTableWidget,
         self.string_list.check_change()
 
     def get_string_list_by_index(self, index: int):
-        if index ==-1:
-            return None
-        elif index < 0 or index >= self.row_count:
-            raise ValueError(f'Index Value {index} Out of Range')
+        if self.index.value<0:
+            return []
         return [self.item(index, col).text() for col in range(self.column_count)]
 
     @property
